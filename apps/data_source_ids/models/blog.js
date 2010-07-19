ce03dda87ab5f28d630032e670ea2092884274c6
Scott Cytacki <scott@concord.org>
2010-07-19 15:17:17 -0400

    result of running: sc-gen model DataSourceIds.Blog

---
+++ apps/data_source_ids/models/blog.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   DataSourceIds.Blog
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals DataSourceIds */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+DataSourceIds.Blog = SC.Record.extend(
+/** @scope DataSourceIds.Blog.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ apps/data_source_ids/tests/models/blog.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   DataSourceIds.Blog Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals DataSourceIds module test ok equals same stop start */
+
+module("DataSourceIds.Blog");
+
+// TODO: Replace with real unit test for Blog
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
