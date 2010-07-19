a5849fa59093042201d59a15ec95bf0947876f75
Scott Cytacki <scott@concord.org>
2010-07-19 15:17:48 -0400

    result of running: sc-gen model DataSourceIds.Post

---
+++ apps/data_source_ids/models/post.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   DataSourceIds.Post
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals DataSourceIds */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+DataSourceIds.Post = SC.Record.extend(
+/** @scope DataSourceIds.Post.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
