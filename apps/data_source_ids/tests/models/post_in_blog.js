4b8f286b1b46916a2773a05cbfc5a7105a68b4fc
Scott Cytacki <scott@concord.org>
2010-07-21 17:44:03 -0400

    Test that shows related id issue
    When the datasource updates the id of the object after create, the datastore doesn't automatically find all the references to the old id and fix them.  This test verifies this situation.

---
+++ apps/data_source_ids/tests/models/post_in_blog.js
@@ -0,0 +1,48 @@
+// ==========================================================================
+// Project:   DataSourceIds.Post Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals DataSourceIds module test ok equals same stop start statusEquals afterStatusChange testAfterPropertyChange*/
+
+// You can run this test directly by going to:
+// http://localhost:4020/data_source_ids/en/current/tests/models/post_in_blog.html
+
+// Note this is wrapped by SC in an anon function so it is safe to define 
+// variables like this.
+var store;
+
+module("DataSourceIds.Post", {
+  setup: function() {
+    
+    // reset fixtures for each test
+    DataSourceIds.Blog.FIXTURES = [
+      { guid: 1,
+        title: 'First Blog'}
+    ];
+    
+    store = SC.Store.create().from('DataSourceIds.MockDataSource');    
+  }
+});
+
+// Basic example of the issue
+test("creating a new post in a blog requires fixing references", function() {
+  var blog = store.find(DataSourceIds.Blog, 1);
+
+  // wait until blog is ready before using it
+  afterStatusChange(blog, function(){
+    var post = store.createRecord(DataSourceIds.Post, {
+      guid: "local_id_1",
+      title: "A new post"
+    });
+    post.set('blog', blog);
+    
+    store.commitRecords();    
+    afterStatusChange(post, function () {
+      ok(post.get('id') !== "local_id_1", "created post record no longer has a local id (it has " + post.get('id') + ")");
+
+      var blogPost = blog.get('posts').objectAt(0);
+      equals(blogPost.get('id'), 'local_id_1', 'blog\'s post still has original id');      
+      ok(blogPost.get('isError'), 'blog\'s post is actually an error: ' + blogPost.get('errorObject'));
+    });
+  });  
+});
