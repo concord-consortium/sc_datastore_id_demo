3e767fba1d33442eb0e01b7a6f8cd53e37276da0
Scott Cytacki <scott@concord.org>
2010-07-21 17:40:05 -0400

    add basic blog to fixtures

--- apps/data_source_ids/fixtures/blog.js
+++ apps/data_source_ids/fixtures/blog.js
@@ -8,28 +8,6 @@ sc_require('models/blog');
 
 DataSourceIds.Blog.FIXTURES = [
 
-  // TODO: Add your data fixtures here.
-  // All fixture records must have a unique primary key (default 'guid').  See 
-  // the example below.
-
-  // { guid: 1,
-  //   firstName: "Michael",
-  //   lastName: "Scott" },
-  //
-  // { guid: 2,
-  //   firstName: "Dwight",
-  //   lastName: "Schrute" },
-  //
-  // { guid: 3,
-  //   firstName: "Jim",
-  //   lastName: "Halpert" },
-  //
-  // { guid: 4,
-  //   firstName: "Pam",
-  //   lastName: "Beesly" },
-  //
-  // { guid: 5,
-  //   firstName: "Ryan",
-  //   lastName: "Howard" }
-
+  { guid: 1,
+    title: 'First Blog'}
 ];
